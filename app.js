d5e4e16bb385b5dafddc10ac9f0c45628a8a8433
Edu Ardo <eduardo.fonseca@sou.unifeob.edu.br>
2024-01-29 17:16:50 -0300

    Desafios

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ alert('Boas vindas ao jogo do número secreto');
 let nsecreto = 5;
 let chute = prompt('Escolha um número entre 1 e 10');
 
-if(chute == nsecreto) {
-    console.log('Isso ai!! Você acertou o número secreto: ', nsecreto);
-} else (chute > nsecreto) {
-    console.log('Que pena! Você errou, o número da sorte é menor que: ', chute);
-} /*end (chute < nsecreto) {
-    console.log('Que pena! Você errou, o número da sorte é maior que: ', chute);
-}/*
+if (chute == nsecreto) {
+    alert(`Isso aí!! Você acertou o número secreto: ${nsecreto} `);
+} else if (chute > nsecreto) {
+    alert(`Que pena! Você errou, o número da sorte é menor que: ${chute}`);
+} else {
+    alert(`Que pena! Você errou, o número da sorte é menor que: ${chute}`);
+}
---
+++ desafios/desafios.js
@@ -0,0 +1,36 @@
+
+//DIA DA SEMANA
+/*let semana = prompt('Em que dia da semana estamos?');
+if((semana == 'sábado' || semana == 'domingo')){
+    alert('Bom final de semana');
+} else alert ('Boa semana!');*/
+
+// positivo ou negativo
+
+/*alert ('Bem vindo ao posito/negativo')
+let nescolha = prompt ('Escolha um número:')
+
+if (nescolha < 0 ){
+    alert(`O número escolhido é Negativo: ${nescolha}`);
+}else alert (`O número escolhido é Positivo: ${nescolha}`);*/
+
+// SISTEMA DE PONTUAÇÃO
+
+/*alert ('Bem vindo ao jogo!')
+let pontos = 100
+let q1 = prompt ('Quantos pontos você tem?')
+if(q1 >= pontos) {
+    alert (`PARABÉNS!! Você ganhou, conseguiu ${q1} pontos.`);
+
+}else {
+    alert (`QUE PENA!! Você perdeu, precisa de ${pontos} pontos.`);
+}*/
+
+
+// SALDO CONTA
+let nome = prompt('Digite seu nome:')
+let saldo = prompt('Digite o saldo da sua conta:')
+let dia = prompt('Que dia é hoje?')
+
+alert (`Olá, ${nome}. Hoje é dia ${dia}, seu saldo é de R$ ${saldo},00.`)
+
